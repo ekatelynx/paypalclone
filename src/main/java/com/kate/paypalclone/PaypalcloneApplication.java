f67abaedaea3eacf7f8784e71d68e6ff85d62e63
Kate <ciborem@gmail.com>
2023-09-22 14:13:30 -0400

    first commit

---
+++ src/main/java/com/kate/paypalclone/PaypalcloneApplication.java
@@ -0,0 +1,13 @@
+package com.kate.paypalclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kate/paypalclone/PaypalcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kate.paypalclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
